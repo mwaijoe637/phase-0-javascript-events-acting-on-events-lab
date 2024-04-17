88c97b895678f8f021e9855c5592ed0248119fdb
joe <brakejoe9@gmail.com>
2024-04-17 12:21:25 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
+  
+  function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger')
+    let left = parseInt(dodger.style.left)
+    
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger')
+    let left = parseInt(dodger.style.left)
+    let dodgerWidth = 40; 
+    
+    if (left + dodgerWidth < 400) { 
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+  
